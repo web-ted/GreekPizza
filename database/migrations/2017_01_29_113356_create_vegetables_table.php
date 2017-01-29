b8fa76ae7170a0d4b4d62bf9fa3ec6e5f12ba86e
Skliris Kostas <test@gmail.com>
2017-01-29 13:35:07 +0200

    Created model and migration for all db tables.

---
+++ database/migrations/2017_01_29_113356_create_vegetables_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVegetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vegetables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vegetables');
+    }
+}
