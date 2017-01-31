1f544d43e6de0f4a84f4aa41418036df3f26656b
Skliris Kostas <test@gmail.com>
2017-01-31 23:38:47 +0200

    Added relations to pizza and order models.

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function customer()
+    {
+        return $this->belongsTo('App\Customer');
+    }
+
+    public function pizza()
+    {
+        return $this->belongsTo('App\Pizza');
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo('App\Employee');
+    }
 }
--- app/Pizza.php
+++ app/Pizza.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pizza extends Model
 {
-    //
+    public function meats()
+    {
+        return $this->belongsToMany('App\Meat');
+    }
+
+    public function sauces()
+    {
+        return $this->belongsToMany('App\Sauce');
+    }
+
+    public function vegetables()
+    {
+        return $this->belongsToMany('App\Vegetable');
+    }
+
+    public function cheeses()
+    {
+        return $this->belongsToMany('App\Cheese');
+    }
 }
