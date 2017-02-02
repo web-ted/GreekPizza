18d3744eb2359b2be1f26e7d30beedc9189cf5c5
tgoltsios@amdtelecom.net <0DrktHhXUpa72x6GJsuZ>
2017-02-02 17:37:29 +0200

    Added routed for meats

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'TestController@test');
+
+Route::get('/meat', 'MeatController@index');
+Route::get('/meat/edit/{id}', 'MeatController@edit');
+Route::post('/meat/update', 'MeatController@update');
+Route::get('/meat/delete/{id}', 'MeatController@remove');
 
 
 Route::get('/order/index', 'OrderController@index');
