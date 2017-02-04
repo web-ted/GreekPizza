bbe48d81fb17582a1407af838b706efafb625beb
Skliris Kostas <test@gmail.com>
2017-02-04 14:39:42 +0200

    Added ingredients method for Pizza model.

--- app/Pizza.php
+++ app/Pizza.php
@@ -8,21 +8,32 @@ class Pizza extends Model
 {
     public function meats()
     {
-        return $this->belongsToMany('App\Meat');
+        return $this->belongsToMany('App\Meat', 'meat_pizza');
     }
 
     public function sauces()
     {
-        return $this->belongsToMany('App\Sauce');
+        return $this->belongsToMany('App\Sauce', 'sauce_pizza');
     }
 
     public function vegetables()
     {
-        return $this->belongsToMany('App\Vegetable');
+        return $this->belongsToMany('App\Vegetable', 'vegetable_pizza');
     }
 
     public function cheeses()
     {
-        return $this->belongsToMany('App\Cheese');
+        return $this->belongsToMany('App\Cheese', 'cheese_pizza');
     }
+
+    public function ingredients()
+    {
+        return [
+            'meats'      => implode(",", $this->meats()->get()->pluck('name')->toArray()),
+            'cheeses'    => implode(",", $this->cheeses()->get()->pluck('name')->toArray()),
+            'vegetables' => implode(",", $this->vegetables()->get()->pluck('name')->toArray()),
+            'sauces'     => implode(",", $this->sauces()->get()->pluck('name')->toArray()),
+        ];
+    }
+
 }
