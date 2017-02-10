c2f63e03316ec5ba5f6a11e5cb55a79b5fdf73e9
tgoltsios@amdtelecom.net <0DrktHhXUpa72x6GJsuZ>
2017-02-10 15:41:13 +0200

    Implemented new authentication system.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="/">Home</a></li>
+            </ol>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>Greek Pizza Web Application</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
+                <p>Welcome to the new Greek Pizza web application. On the top meu you can browse to the application components.
+                    You can create new Pizzas add new ingredients and make orders.</p>
+            </div>
+        </div>
+    </div>
+@endsection
