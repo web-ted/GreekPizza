2acb37af9d98ffc89881ebdcf1126e1d7e42a1a9
Skliris Kostas <test@gmail.com>
2017-02-11 14:41:32 +0200

    Unguarded customers model to allow mass assignment to all fields.

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     * This means all.
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
