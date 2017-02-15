045437fbfdad28a084fe7730c191816b856737ad
Skliris Kostas <test@gmail.com>
2017-02-15 06:05:53 +0200

    Show user role in main layout while logged in.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -52,7 +52,7 @@
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            {{ Auth::user()->name }} ({{ Auth::user()->role }}) <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
