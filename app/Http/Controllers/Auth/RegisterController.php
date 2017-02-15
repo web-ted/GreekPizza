c586f4cfcb01da40356fac26eb5a3da92664e7df
Skliris Kostas <test@gmail.com>
2017-02-15 22:20:48 +0200

    Show user role in main layout while logged in.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,6 @@ class RegisterController extends Controller
         $user = User::create([
             'name'     => $data['firstname'] . ' ' . $data['lastname'],
             'email'    => $data['email'],
-            'role'     => 'customer',
             'password' => bcrypt($data['password']),
             'role'     => 'customer',
         ]);
